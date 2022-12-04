3e96a256bdde9b4d5608a7ae7d28f6fd75c65dda
Sebastian Ross <sebastian.ross@warnermedia.com>
2022-12-04 09:55:48 +0100

    day4 cleanup

--- 04.mjs
+++ 04.mjs
@@ -6,26 +6,24 @@ const lines = (await readFile("./04.txt", { encoding: "UTF8" }))
 
 const inRange = (x, min, max) => (x - min) * (x - max) <= 0;
 
-const isBothOverlap = (first, second) =>
-  (inRange(first[0], second[0], second[1]) &&
-    inRange(first[1], second[0], second[1])) ||
-  (inRange(second[0], first[0], first[1]) &&
-    inRange(second[1], first[0], first[1]));
-
-const isSomeOverlap = (first, second) =>
-  inRange(first[0], second[0], second[1]) ||
-  inRange(first[1], second[0], second[1]) ||
-  inRange(second[0], first[0], first[1]) ||
-  inRange(second[1], first[0], first[1]);
+const isOverlap = (first, second, fn) =>
+  fn(
+    inRange(first[0], second[0], second[1]),
+    inRange(first[1], second[0], second[1])
+  ) ||
+  fn(
+    inRange(second[0], first[0], first[1]),
+    inRange(second[1], first[0], first[1])
+  );
 
 const nrOfOverlap = lines.reduce((total, line) => {
   const [first, second] = line.split(",").map((pair) => pair.split("-"));
-  return isBothOverlap(first, second) ? ++total : total;
+  return isOverlap(first, second, (a, b) => a && b) ? ++total : total;
 }, 0);
 
 const nrOfSomeOverlap = lines.reduce((total, line) => {
   const [first, second] = line.split(",").map((pair) => pair.split("-"));
-  return isSomeOverlap(first, second) ? ++total : total;
+  return isOverlap(first, second, (a, b) => a || b) ? ++total : total;
 }, 0);
 
 console.log(`Answer 1: ${nrOfOverlap}`);
